10c8edd5f73739cd4874bd432029cdf320f19b4f
Hunter <imset@users.noreply.github.com>
2022-05-10 11:27:43 -0700

    cleanup fightan

--- lib/cogs/fightan.py
+++ lib/cogs/fightan.py
@@ -3,7 +3,6 @@ import asyncio
 from mediawiki import (
     MediaWiki, exceptions
 )
-
 from discord import Embed, Member
 from discord.errors import HTTPException
 from discord.ext.commands import (
@@ -20,30 +19,30 @@ class Fightan(Cog):
         self.bot = bot
         
 
-    @command(name="strive",
-            aliases=["ggs"],
-            brief="This command does not yet function",
-            hidden=True,
-            enabled=False)
-    async def strive_search(self, ctx, *, page: str):
-        dl = MediaWiki(user_agent='py-Dizzi-Discord-Bot',url="http://www.dustloop.com/wiki/api.php")
-        message = await ctx.send(f"Searching {page.title()} on Strive Dustloop")
+    # @command(name="strive",
+    #         aliases=["ggs"],
+    #         brief="This command does not yet function",
+    #         hidden=True,
+    #         enabled=False)
+    # async def strive_search(self, ctx, *, page: str):
+    #     dl = MediaWiki(user_agent='py-Dizzi-Discord-Bot',url="http://www.dustloop.com/wiki/api.php")
+    #     message = await ctx.send(f"Searching {page.title()} on Strive Dustloop")
         
-        try:
-            p = dl.page(title=f"GGST/{page}", preload=True)
-            pgtitle = p.title
-        except:
-            p = dl.page(title=dl.search(f"GGST {page}"), preload=True)
-            pgtitle = p.title + " (Closest Match to \"" + page.title() + "\")"
+    #     try:
+    #         p = dl.page(title=f"GGST/{page}", preload=True)
+    #         pgtitle = p.title
+    #     except:
+    #         p = dl.page(title=dl.search(f"GGST {page}"), preload=True)
+    #         pgtitle = p.title + " (Closest Match to \"" + page.title() + "\")"
             
-        embed = Embed(title=f"{pgtitle}", description=p.content.summarize(chars=300), color=DIZZICOLOR)
-        embed.add_field(name="Read More", value=f"[Dustloop](https://www.dustloop.com/?curid={p.pageid})", inline=False)
-        try:
-            embed.set_thumbnail(url=p.logos[0])
-        except:
-            pass
+    #     embed = Embed(title=f"{pgtitle}", description=p.content.summarize(chars=300), color=DIZZICOLOR)
+    #     embed.add_field(name="Read More", value=f"[Dustloop](https://www.dustloop.com/?curid={p.pageid})", inline=False)
+    #     try:
+    #         embed.set_thumbnail(url=p.logos[0])
+    #     except:
+    #         pass
         
-        updated_member = await message.edit(content="", embed=embed)
+    #     updated_member = await message.edit(content="", embed=embed)
 
     @Cog.listener()
     async def on_ready(self):
